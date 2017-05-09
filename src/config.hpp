4b8426dc5e3a5e05210b3e5dcdbb6b49db6537dd
Sam Brkopac <brkopac@gmail.com>
2017-05-09 14:45:42 -0700

    added default values for getters
    added default values for getters

--- src/config.hpp
+++ src/config.hpp
@@ -14,10 +14,10 @@ namespace ehb
             Config(int * argc, char * argv[]);
            ~Config();
 
-            static bool getBool(const std::string & key, bool defaultValue);
-            static float getFloat(const std::string & key, float defaultValue);
-            static int getInt(const std::string & key, int defaultValue);
-            static const std::string & getString(const std::string & key, const std::string & defaultValue);
+            static bool getBool(const std::string & key, bool defaultValue = false);
+            static float getFloat(const std::string & key, float defaultValue = 0.0f);
+            static int getInt(const std::string & key, int defaultValue = 0);
+            static const std::string & getString(const std::string & key, const std::string & defaultValue = "");
 
             static void dump();
             static void dump(std::ostream & stream);
